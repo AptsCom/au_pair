c261c9485190db614141a3aaf595f882d9746d0b
Bantik <corey@idolhands.com>
2013-10-31 10:21:04 -0500

    Test tweaks

--- spec/authentication_token_spec.rb
+++ spec/authentication_token_spec.rb
@@ -5,15 +5,15 @@ describe AuPair::AuthenticationToken do
   context 'token validation from config' do
 
     before do
-      AuPair.configure{ |config| config.tokens = [{:vendor => 'foo', :key => '1234'}, {:vendor => 'bar', :key => '5678'}]}
+      AuPair.configure{ |config| config.tokens = {'foo' => '1234', 'bar' => '5678'} }
     end
 
     it 'identifies a valid token' do
-      expect(AuPair::AuthenticationToken.validate('foo', '1234')).to be_true
+      expect(AuPair::AuthenticationToken.valid?('1234', 'foo')).to be_true
     end
 
     it 'detects an invalid token' do
-      expect(AuPair::AuthenticationToken.validate('foo', '5678')).to be_false
+      expect(AuPair::AuthenticationToken.valid?('4567', 'foo')).to be_false
     end
 
   end
